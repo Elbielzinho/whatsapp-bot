4601fbc91e53323d6b77119c998d922d34279ab2
Elbielzinho <59930738+Elbielzinho@users.noreply.github.com>
2021-03-25 20:05:16 -0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const { create, Client } = require('@open-wa/wa-automate')
+const welcome = require('./lib/welcome')
+const msgHandler = require('./msgHndlr')
+const options = require('./options')
+
+const start = async (client = new Client()) => {
+        console.log('[SERVER] Server Started!')
+        // Force it to keep the current session
+        client.onStateChanged((state) => {
+            console.log('[Client State]', state)
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
+        })
+        // listening on message
+        client.onMessage((async (message) => {
+            client.getAmountOfLoadedMessages()
+            .then((msg) => {
+                if (msg >= 3000) {
+                    client.cutMsgCache()
+                }
+            })
+            msgHandler(client, message)
+        }))
+
+        client.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(client, heuh)
+            //left(client, heuh)
+            }))
+        
+        client.onAddedToGroup(((chat) => {
+            let totalMem = chat.groupMetadata.participants.length
+            if (totalMem < 30) { 
+            	client.sendText(chat.id, `O membro é apenas ${totalMem}, se você quiser convidar o bot, o número mínimo de mem é 30`).then(() => client.leaveGroup(chat.id)).then(() => client.deleteChat(chat.id))
+            } else {
+                client.sendText(chat.groupMetadata.id, `Olá membros do grupo *${chat.contact.name}* obrigado por convidar este bot, para ver o menu envie *! help*`)
+            }
+        }))
+
+        /*client.onAck((x => {
+            const { from, to, ack } = x
+            if (x !== 3) client.sendSeen(to)
+        }))*/
+
+        // listening on Incoming Call
+        client.onIncomingCall(( async (call) => {
+            await client.sendText(call.peerJid, 'Não consigo receber chamadas.  Tu levou foi um lindo block, chame eu mais tarde para dar os desblock!')
+            .then(() => client.contactBlock(call.peerJid))
+        }))
+    }
+
+create(options(true, start))
+    .then(client => start(client))
+    .catch((error) => console.log(error))
